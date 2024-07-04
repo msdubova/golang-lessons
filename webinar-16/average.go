d9be4df4e232c99235c9c20dc2b78c9e87864fac
ollevche <ollevche@gmail.com>
2024-07-04 20:33:51 +0300

    add more subtests
    Signed-off-by: ollevche <ollevche@gmail.com>

--- webinar-16/average.go
+++ webinar-16/average.go
@@ -1,9 +1,14 @@
 package main
 
 func Average(elems []int) int {
+	if len(elems) == 0 {
+		return 0
+	}
+
 	var sum int
 	for _, e := range elems {
 		sum += e
 	}
+
 	return sum / len(elems)
 }
--- webinar-16/average_test.go
+++ webinar-16/average_test.go
@@ -1,13 +1,43 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAverage(t *testing.T) {
-	input := []int{1, 2, 3, 10}
-	expectedOutput := 4
+	tests := []struct {
+		input          []int
+		expectedOutput int
+	}{
+		{
+			input:          []int{1, 2, 3, 10},
+			expectedOutput: 4,
+		},
+		{
+			input:          []int{10},
+			expectedOutput: 10,
+		},
+		{
+			input:          []int{100, 200, 3, 10},
+			expectedOutput: 78,
+		},
+		{
+			input:          []int{-100, 200, -3, 10},
+			expectedOutput: 26,
+		},
+		{
+			input:          []int{},
+			expectedOutput: 0,
+		},
+	}
 
-	actualOutput := Average(input)
-	if actualOutput != expectedOutput {
-		t.Fatalf("Got unexpected result: %v. Want: %v", actualOutput, expectedOutput)
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v", test.input), func(t *testing.T) {
+			actualOutput := Average(test.input)
+			if actualOutput != test.expectedOutput {
+				t.Fatalf("Got unexpected result: %v. Want: %v", actualOutput, test.expectedOutput)
+			}
+		})
 	}
 }
